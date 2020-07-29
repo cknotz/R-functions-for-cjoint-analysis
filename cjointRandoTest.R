c87a9d3a004d4ab96f2c4a88e6d0c7cae6fc88cc
cknotz <carlo.knotz@gmail.com>
2020-07-29 09:20:54 +0200

    Improved output & removed comments from code; no change to functionality

--- cjointRandoTest.R
+++ cjointRandoTest.R
@@ -1,14 +1,9 @@
 cjointRandoTest <- function(data,resvars,dims) {
-  
-# data: data.frame
-  # resvars: character vector of respondent characteristics
-  # dims: character vector of profile dimension
-  
-# Runs chi-squared test first over combinations of resp-vars and profile dimensions
+
 invisible(sapply(resvars,function(x){
   invisible(sapply(dims, function(y){
     test <- chisq.test(data[[x]],data[[y]])
-        print(paste0("Respondent variable = ",x,"; Profile variable = ",y,"; df = ",test$parameter, "; p-value = ",round(test$p.value,3)))
+        cat(paste0("\nRespondent variable = ",x,"; Profile variable = ",y,";\n Chi-sq.:",round(test$statistic, digits = 3),"; df = ",test$parameter, "; p-value = ",round(test$p.value,3)))
   }))
-})) 
+}))
 }
