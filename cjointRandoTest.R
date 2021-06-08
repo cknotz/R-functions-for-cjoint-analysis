eda5a02c3eaed580d772c0e043fccb0ef37649d8
cknotz <carlo.knotz@gmail.com>
2021-06-08 10:48:01 +0200

    Fixed error in rando test

--- cjointRandoTest.R
+++ cjointRandoTest.R
@@ -2,7 +2,7 @@ cjointRandoTest <- function(data,resvars,dims) {
 
 invisible(sapply(resvars,function(x){
   invisible(sapply(dims, function(y){
-    test <- chisq.test(data[[x]],data[[y]])
+    test <- chisq.test(table(data[[x]],data[[y]]))
         cat(paste0("\nRespondent variable = ",x,"; Profile variable = ",y,";\n Chi-sq.:",round(test$statistic, digits = 3),"; df = ",test$parameter, "; p-value = ",round(test$p.value,3)))
   }))
 }))
